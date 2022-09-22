abec87e5e1d9b651b515b9ab037574875bb8a24d
dunghenry <trandungksnb6930082001>
2022-09-22 10:47:13 +0700

    add bucket paging

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const comment = require('./comment.route');
 const club = require('./club.route');
 const player = require('./player.route');
 const event = require('./event.route');
+const bucket = require('./bucket.route');
 const routes = (app) => {
     app.use('/api', comment);
     app.use('/api', club);
     app.use('/api', player);
     app.use('/api', event);
+    app.use('/api', bucket);
 };
 
 module.exports = routes;
